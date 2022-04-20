e0420fc18814cec2cd94b398b399cc297d450467
Guido <guidojulianou@gmail.com>
2022-04-20 15:15:10 -0300

    20/04

--- visualizador_cc/static/js/project.js
+++ visualizador_cc/static/js/project.js
@@ -1,3 +1,24 @@
 const btnSwitch = document.querySelector('#switch');
 
-btnSwitch.addEventListener();
+btnSwitch.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active');
+
+    //guardar el modo en localstorage
+    if(document.body.classList.contains('dark')){
+        localStorage.setItem('dark-mode', 'true');
+    } else {
+        localStorage.setItem('dark-mode', 'false');
+    }
+});
+
+//obtener el modo actual
+if(localStorage.getItem('dark-mode') == 'true'){
+    document.body.classList.add('dark');
+    btnSwitch.classList.add('active');
+
+} else{
+    document.body.classList.remove('dark');
+    btnSwitch.classList.remove('active');
+
+}
