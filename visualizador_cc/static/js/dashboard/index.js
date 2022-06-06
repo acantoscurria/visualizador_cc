ed0e543d66dc909965b0e5bebaf8ecefe1190b93
jose.tablate <jose.tablate@gmail.com>
2022-06-05 23:23:52 -0300

    sdfsd

--- visualizador_cc/static/js/dashboard/index.js
+++ visualizador_cc/static/js/dashboard/index.js
@@ -18,32 +18,13 @@ function getColorsList(n){
 
 }
 
-const ctx = document.getElementById('chart_bar_oferta_educativa').getContext('2d');
-
 const colorList = getColorsList(18)
 
-var data = {
-    "R.E.1": 141,  
-    "SUB. R.E. 1": 46, 
-    "R.E. 2": 380,  
-    "SUB. R.E. 2": 135,  
-    "R.E. 3": 146,  
-    "SUB. R.E. 3": 55,  
-    "R.E. 4-A": 257,  
-    "R.E. 4-B": 278,  
-    "R.E. 5": 172,  
-    "SUB. R.E. 5": 129,  
-    "R.E. 6": 136,  
-    "R.E. 7": 170,  
-    "R.E. 8-A": 289,  
-    "R.E. 8-B": 145,  
-    "R.E. 9": 423,  
-    "R.E. 10-A": 357,  
-    "R.E. 10-B": 356,  
-    "R.E. 10-C": 341   
-}
+//chart_bar_oferta_educativa
 
-const myChart = new Chart(ctx, {
+const div_chart_bar_oferta_educativa = document.getElementById('chart_bar_oferta_educativa').getContext('2d');
+
+const myChart_chart_bar_oferta_educativa = new Chart(div_chart_bar_oferta_educativa, {
     type: 'bar',
     data: {
         labels: [
@@ -67,7 +48,7 @@ const myChart = new Chart(ctx, {
             'R.E. 10-C',           
         ],
         datasets: [{
-            label: '# of Votes',
+            label: 'Ofertas',
             data: [
                 141, 
                 46, 
@@ -101,3 +82,70 @@ const myChart = new Chart(ctx, {
         }
     }
 });
+
+
+//chart_bar_matricula
+
+const div_chart_bar_matricula = document.getElementById('chart_bar_matricula').getContext('2d');
+
+const myChart_chart_bar_matricula = new Chart(div_chart_bar_matricula, {
+    type: 'bar',
+    data: {
+        labels: [
+            'R.E.1', 
+            'SUB. R.E. 1',
+            'R.E. 2', 
+            'SUB. R.E. 2', 
+            'R.E. 3', 
+            'SUB. R.E. 3', 
+            'R.E. 4-A', 
+            'R.E. 4-B', 
+            'R.E. 5', 
+            'SUB. R.E. 5', 
+            'R.E. 6', 
+            'R.E. 7', 
+            'R.E. 8-A', 
+            'R.E. 8-B', 
+            'R.E. 9', 
+            'R.E. 10-A', 
+            'R.E. 10-B', 
+            'R.E. 10-C',           
+        ],
+        datasets: [{
+            label: 'Mátriculas',
+            data: [
+                7198, 
+                1804, 
+                26933, 
+                9221, 
+                14553, 
+                4455, 
+                37545, 
+                31492, 
+                17945, 
+                9668, 
+                10471, 
+                15090, 
+                32181, 
+                10231, 
+                42750, 
+                68848, 
+                65573, 
+                46547, 
+                 
+            ],
+            backgroundColor: colorList,
+            borderColor: colorList,
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+
