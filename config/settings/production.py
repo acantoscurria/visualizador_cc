e9e5ab7a8ab42a4f506d0a8e62714e6ed8fbbd61
Juan Ledesma <juanjoseledesma90@gmail.com>
2023-03-27 12:16:03 -0300

    cambios por dominio nuevo

--- config/settings/production.py
+++ config/settings/production.py
@@ -12,7 +12,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["estadisticaschaco.ga"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["visoreducativochaco.com.ar"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
@@ -65,7 +65,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
     "DJANGO_DEFAULT_FROM_EMAIL",
-    default="visualizador_cc <noreply@estadisticaschaco.ga>",
+    default="visualizador_cc <noreply@visoreducativochaco.com.ar>",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
